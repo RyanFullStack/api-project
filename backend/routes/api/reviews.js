7574d332d5e9ab08a0f5f8d046be9871403a42f8
Ryan <ryanjerickson@gmail.com>
2023-06-13 15:33:02 -0500

    Add Image to Review by Review Id

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -21,7 +21,9 @@ router.get('/current', requireAuth, async (req, res, next) => {
         newData.push(spotObj)
         })
     newData.forEach(spot => {
+        if (spot.Spot) {
         spot.Spot.previewImage = 'No Images'
+        }
         if (spot.ReviewImages.length) {
             spot.ReviewImages.forEach(review => {
                 spot.Spot.previewImage = review.url
@@ -34,6 +36,38 @@ router.get('/current', requireAuth, async (req, res, next) => {
 })
 
 
+router.post('/:reviewId/images', requireAuth, async(req, res, next) => {
+    const review = await Review.findByPk(req.params.reviewId)
+    if (!review) {
+        const err = new Error(`Review couldn't be found`)
+        err.status = 404
+        next(err)
+    }
+
+    const revImgCount = await ReviewImage.count({
+        where: {
+            reviewId: req.params.reviewId
+        }
+    })
+    if (revImgCount >= 10) {
+        res.status(403)
+        res.json({message: 'Maximum number of images for this resource was reached'})
+    }
+
+    if (review.userId === req.user.id) {
+        const { url } = req.body
+        const newReviewImage = await ReviewImage.create({
+            reviewId: req.params.reviewId,
+            url
+        })
+        const id = newReviewImage.id
+
+        res.json({id, url})
+    }
+
+    res.status(403)
+    res.json({message: 'Forbidden'})
+})
 
 
 module.exports = router;
