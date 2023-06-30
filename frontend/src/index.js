bf711383b7cfcbebe698ce4aea002baba9bc10c6
Ryan <ryanjerickson@gmail.com>
2023-06-30 13:27:47 -0500

    Modals Working Good Commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
-
 import "./index.css";
-
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { ModalProvider, Modal } from "./context/Modal";
 import App from "./App";
-
 import configureStore from "./store";
 import { restoreCSRF, csrfFetch } from "./store/csrf";
 import * as sessionActions from "./store/session";
